db1993ed37af1181a6a4b866cb7aade6845b23a3
wvengen <dev-voko@willem.engen.nl>
2019-03-20 14:06:16 +0100

    Setup members mail send infrastructure (#4)

--- members/docker-entrypoint.sh
+++ members/docker-entrypoint.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
+# Update variables in the members app
 sed -E -i "s#^(sqlalchemy\\.url\\s*=\\s*).*\$#\\1mysql+pymysql://members:${MEMBERS_DB_PASSWORD}@mariadb/members#" production.ini
 sed -E -i "s#^(vokomokum\\.whitelist_came_from\\s*=\\s*).*\$#\\1${REDIRECT_URLS}#" production.ini
 sed -E -i "s#^(vokomokum\\.client_secret\\s*=\\s*).*\$#\\1${VOKOMOKUM_CLIENT_SECRET}#" production.ini
+sed -E -i "s#^(vokomokum\\.mail_sender\\s*=\\s*).*\$#\\1${EMAIL_SENDER}#" production.ini
 sed -E -i "s#request\\.application_url#'${APPLICATION_URL}'#" members/templates/base.pt
+
+sed -E -i "s#(hostname\\s*=\\s*)#\\1${HOSTNAME}#" /etc/ssmtp/ssmtp.conf
 
 echo "Running: $@"
 exec "$@"
